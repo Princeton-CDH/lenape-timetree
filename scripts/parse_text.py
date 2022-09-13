dd666b72273cbd03c6a435cd5464f2cd688fb813
rlskoeser <rebecca.s.koeser@princeton.edu>
2022-09-13 16:26:26 -0400

    Handle multiple input files when parsing leaf data #3

--- scripts/parse_text.py
+++ scripts/parse_text.py
@@ -17,16 +17,27 @@ def parse_file(filepath):
             line = line.strip()
 
             # blank line indicates section break; save any parsed data
-            if line  == "":
+            if line == "":
                 if data:
                     datapoints.append(data)
                     data = {}
 
             elif ":" in line:
-                label, value = line.split(':')
+                label, value = line.split(":")
                 # convert readable label to data variable
-                label = label.strip().lower().replace(' ', '_')
+                label = label.strip().lower().replace(" ", "_")
                 value = value.strip()
+
+                # records may include a colon in the text; if the label
+                # is not one we know about, assume the line is text
+                if label not in [
+                    "branch",
+                    "display_date",
+                    "sort_date",
+                    "tags",
+                    "title",
+                ]:
+                    data["text"] = line.strip()
 
                 # split tags out into a list
                 if label == "tags":
@@ -40,11 +51,10 @@ def parse_file(filepath):
                     except ValueError:
                         pass
 
-
                 data[label] = value
             # non-empty line without : is text content
             else:
-                data['text'] = line
+                data["text"] = line
 
         # add last section to list
         if data:
@@ -53,33 +63,37 @@ def parse_file(filepath):
     return datapoints
 
 
-def generate_leaf_pages(datapoints):
+def generate_leaf_pages(datapoints, offset=0):
     # expects a list of dictionaries
     for i, data in enumerate(datapoints):
         # TODO: need short names to use for file names & page titles
-        title = data.get('title', '')
-        slug = title.lower().replace(' ', '-')
+        title = data.get("title", "")
+        slug = title.lower().replace(" ", "-").replace(".", "").replace(",", "")
         if not slug:
-            # use index as fallback for now now
-            slug = '%s-leaf' % i
+            # use index + offset as fallback filename for now
+            slug = "%s-leaf" % (i + offset)
 
-        with open('content/leaves/%s.md' % slug, 'w') as outfile:
+        with open("content/leaves/%s.md" % slug, "w") as outfile:
             # remove text from the dictionary
-            text = data.pop('text')
+            text = data.pop("text")
             # output remaining info as yaml
             yaml.dump(data, explicit_start=True, stream=outfile)
             # then finish the content file with text
-            outfile.write('---\n\n%s' % text)
+            outfile.write("---\n\n%s" % text)
 
 
-
-if __name__ ==  "__main__":
-    try:
-        input_file = sys.argv[1]
-    except IndexError:
-        print('Input file is required')
+if __name__ == "__main__":
+    input_files = sys.argv[1:]
+    if not input_files:
+        print("At least one input file is required")
         exit(-1)
 
-
-    datapoints = parse_file(sys.argv[1])
-    generate_leaf_pages(datapoints)
+    leaf_offset = 0
+    # handle multiple input files; use an offset to ensure
+    # numeric filenames don't collide
+    for infile in input_files:
+        print("Processing %s" % infile)
+        datapoints = parse_file(infile)
+        generate_leaf_pages(datapoints, offset=leaf_offset)
+        print("  parsed %d datapoints" % len(datapoints))
+        leaf_offset += len(datapoints)
