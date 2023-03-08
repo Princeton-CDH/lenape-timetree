d5d3f759c562029ba73a2f9f4ca3c497b8a2537b
Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
2023-03-08 10:15:18 -0500

    Support for leaf display title & import latest leaf data (#99)
    * Add display title field to input text parsing script #94
    
    * Revise pre-commit config to not prettify hugo templates for json
    
    * Include display title in leaf json data but only when set #94
    
    * Use display title for label in tree if set, otherwise use title #94
    
    * Document display title in contributor instructions
    
    * Handle empty display title field with no space after colon
    
    * Import updated leaves with display titles and consolidated tags
    
    * Restore three figures added previously
    
    * Remove redundant leaf files
    
    * Restore three figures added previously
    
    * Improve handling for entry with missing text
    
    * Split tag that was mistakenly not separated

--- scripts/parse_text.py
+++ scripts/parse_text.py
@@ -24,6 +24,10 @@ def parse_file(filepath):
                     datapoints.append(data)
                     data = {}
 
+            # skip empty display title with : and no space
+            elif line.endswith(":"):
+                continue
+
             # look for colon in first few characters, indicating a labeled field
             elif ": " in line[:20]:
                 # only split once
@@ -41,6 +45,7 @@ def parse_file(filepath):
                     "sort_date",
                     "tags",
                     "title",
+                    "display_title",
                 ]:
                     data["text"] = line.strip()
 
@@ -72,8 +77,11 @@ def generate_leaf_pages(datapoints, offset=0):
     # expects a list of dictionaries
     for i, data in enumerate(datapoints):
         # NOTE: need short names to use for file names & page titles,
-        # not all records have them yet
-        title = data.get("title", "")
+        # but not all records have them yet
+        # use display title first, if set, since it will be shorter
+        title = data.get("display_title", "")
+        if not title:
+            title = data.get("title", "")
         # slugify title to use as basis for filename
         slug = slugify(title)
         if not slug:
@@ -85,6 +93,7 @@ def generate_leaf_pages(datapoints, offset=0):
             try:
                 text = data.pop("text")
             except KeyError:
+                text = ""  # don't use text from preceeding entry
                 print("no text")
                 print(data)
             # output remaining info as yaml
