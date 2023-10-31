118e4a32395f13d0c6583fe7443d5f1206023a2c
rlskoeser <rebecca.s.koeser@princeton.edu>
2023-10-31 17:03:15 -0400

    Use script tag to embed json data, per code review feedback

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 import { TimeTree } from "./timetree";
 
 // load & parse leaf data from json embedded in the document
-const data = JSON.parse(document.querySelector(".leaf-data").value);
+const data = JSON.parse(document.getElementById("leaf-data").text);
 // load and parse tag list for label / slug lookup label based on slug
-const tags = JSON.parse(document.querySelector(".tag-data").value);
+const tags = JSON.parse(document.getElementById("tag-data").text);
 // determine if this is a production or development build
-const params = JSON.parse(document.querySelector(".env-data").value);
+const params = JSON.parse(document.getElementById("env-data").text);
 
 // pass in leaf data, tag list, and whether debugging should be enabled
 let timetree = new TimeTree(data, tags, params);
